03ac06c26bf432707843b890163fb2766016708b
Daniel Müller <danielmueller@limebird.de>
2019-09-25 22:35:12 +0200

    enough for today

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -7,7 +7,7 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    return String::from("blue");
 }
 
 
--- exercises/test1.rs
+++ exercises/test1.rs
@@ -9,7 +9,13 @@
 
 // Put your function here!
 // fn ..... {
-
+fn calculate_price(price: i32) -> i32 {
+    if (price > 40) {
+        return price;
+    } else {
+        return price * 2;
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
